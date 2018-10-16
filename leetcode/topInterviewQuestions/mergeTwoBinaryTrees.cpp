d7860afdd7fea5db6e347b0befc67e25e5f04572
Pedro <pedro.mendes.unicamp@gmail.com>
2018-10-16 02:48:51 +0000

    adding new solution

---
+++ leetcode/topInterviewQuestions/mergeTwoBinaryTrees.cpp
@@ -0,0 +1,52 @@
+/* https://leetcode.com/problems/merge-two-binary-trees/submissions/ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/* Recursive O(N)*/
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(t1 == NULL && t2!=NULL){
+            t1 = new TreeNode(t2->val);
+            t1 ->left = mergeTrees(t1->left, t2->left);
+            t1 -> right = mergeTrees(t1->right, t2->right);
+            return t1;
+        }else if(t1!= NULL && t2 == NULL){
+            return t1;
+        }else if(t1 == NULL && t2==NULL){
+            return t1;
+        }else{
+            t1->val = t1->val + t2->val;
+            t1->left = mergeTrees(t1 -> left, t2->left);
+            t1->right = mergeTrees(t1->right, t2->right);
+            return t1;
+        }
+    }
+};
+
+/*Removing unnecessary statements: O(N)*/
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(t1 == NULL && t2!=NULL){
+            t1 = new TreeNode(t2->val);
+            t1 ->left = mergeTrees(t1->left, t2->left);
+            t1 -> right = mergeTrees(t1->right, t2->right);
+            return t1;
+        }else if(t1 != NULL && t2!=NULL){
+            t1->val = t1->val + t2->val;
+            t1->left = mergeTrees(t1 -> left, t2->left);
+            t1->right = mergeTrees(t1->right, t2->right);
+        }
+        return t1;
+    }
+};
