c71ee067bcd7f1f63b5579f9d2d6275e508d212e
Pedro-Mendes <pedro.mendes.unicamp@gmail.com>
2018-10-19 17:42:53 -0300

    correcting ex 101

--- leetcode/topInterviewQuestions/easy/symmetricTree.cpp
+++ leetcode/topInterviewQuestions/easy/symmetricTree.cpp
@@ -1,22 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+/* https://leetcode.com/problems/symmetric-tree/ 
+git@Pedro-Mendes*/
+
 class Solution {
 public:
     stringstream ss;
     string in;
-    void travLeft(TreeNode* root, vector<string> nodes){
+    void travLeft(TreeNode* root, vector<string> &nodes){
         if(root == nullptr){
             nodes.push_back("");
             return;
         }else{
             ss.str("");
+            //cout << "val = " << root->val << endl;
             ss << root->val;
             in = ss.str();
             nodes.push_back(in);
@@ -37,8 +32,8 @@ public:
                 return;
             }
             i+=1;
-            travLeft(root->right,nodes);
-            travLeft(root->left,nodes);    
+            travRight(root->right,nodes,i,answer);
+            travRight(root->left,nodes,i,answer);    
         }
         
     }
@@ -46,14 +41,39 @@ public:
     bool isSymmetric(TreeNode* root) {
         if(root == nullptr){
             return true;
+        }else if((root->right == nullptr && root->left) || (root->left == nullptr && root->right)){
+            return false;
         }else{
             int i=0;
             vector<string> nodes;
             bool answer = true;
             travLeft(root->left, nodes);
+            //for(int j=0; j<nodes.size();j++)
+                //cout << nodes[j] << " ";
+            //cout << endl;
             travRight(root->right, nodes,i,answer);
+            if(i<nodes.size()-1){
+                return false;
+            }
             return answer;    
         }
         
+    }
+};
+
+
+/* Optimized solution*/
+
+class Solution {
+public:
+    bool isMirror(TreeNode* t1, TreeNode* t2) {
+        if (t1 == nullptr && t2 == nullptr) return true;
+        if (t1 == nullptr || t2 == nullptr) return false;
+        return (t1->val == t2->val)
+            && isMirror(t1->right, t2->left)
+            && isMirror(t1->left, t2->right);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root, root);
     }
 };
