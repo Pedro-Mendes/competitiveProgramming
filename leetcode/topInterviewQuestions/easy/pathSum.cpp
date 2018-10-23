e3193af027d79d738ef3c487c5fc25c994c0cfbe
Pedro <pedro.mendes.unicamp@gmail.com>
2018-10-23 02:39:42 +0000

    adding optimized example pathSum

--- leetcode/topInterviewQuestions/easy/pathSum.cpp
+++ leetcode/topInterviewQuestions/easy/pathSum.cpp
@@ -20,3 +20,43 @@ public:
         return answer;
     }
 };
+
+/*Awesome solution by karan14
+https://leetcode.com/problems/path-sum/discuss/182269/C%2B%2B-using-recursion-beats-100*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        
+        if(!root)
+            return false;
+        if(!root->left && !root->right && sum==root->val)
+            return true;
+            
+        if(root->left && root->right)
+        {
+            return hasPathSum(root->left,sum - root->val) || hasPathSum(root->right,sum - root->val);
+        }
+        else if(!root->right){
+            return hasPathSum(root->left,sum - root->val);}
+        else if(!root->left){
+            return hasPathSum(root->right,sum - root->val);
+        }
+        
+       
+        
+        
+        return false;
+        
+        
+    }
+};
