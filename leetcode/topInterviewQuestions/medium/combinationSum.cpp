5ddb347cd49611847fef299ea66ebbfd477e90ab
Pedro <pedro.mendes.unicamp@gmail.com>
2018-10-25 02:54:04 +0000

    correcting and optimizing combinationSum

--- leetcode/topInterviewQuestions/medium/combinationSum.cpp
+++ leetcode/topInterviewQuestions/medium/combinationSum.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    void combination (int index, const int &n, int k, vector<vector<int>> &solution, const int &target, vector<int> candidates, vector<int> &step, int &sum) {
-        if(k==0){
-            if(sum == target)
-                solution.push_back(step);
+    void combination (int index, const int &n, vector<vector<int>> &solution, const int &target, vector<int> candidates, vector<int> &step, int &sum) {
+        if (sum==target){
+            solution.push_back(step);
             return;
         }
-        for(int i = index; i<=n-k;i++){
+        if(sum>target)
+            return;
+        for(int i = index; i<n;i++){
             step.push_back(candidates[i]);
             sum+=candidates[i];
-            combination(i+1,n,k-1,solution,target,candidates,step,sum);
+            combination(i,n,solution,target,candidates,step,sum);
             step.pop_back();
             sum-=candidates[i];
         }
@@ -21,9 +22,37 @@ public:
         int sum = 0;
         vector<int> step;
         vector<vector<int>> solution;
-        for (int k=1; k<=n; k++) {
-            combination(0,n,k,solution,target,candidates,step,sum);
+        combination(0,n,solution,target,candidates,step,sum);
+        return solution;
+    }
+};
+
+/*Optimization*/
+class Solution {
+public:
+    void combination (vector<vector<int>> &solution, const vector<int> &candidates, int target, int index, vector<int> &step, const int &n) {
+        if(target==0){
+            solution.push_back(step);
+            return;
         }
+        if(target<0)
+            return;
+        for(int i=index;i<n;i++){
+            int c = candidates[i];
+            if(c<=target){
+                step.push_back(c);
+                combination(solution, candidates, target-c,i,step,n);
+                step.pop_back();
+            }
+        }
+            
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> solution;
+        vector<int> step;
+        int n = candidates.size();
+        combination(solution, candidates,target,0,step,n);
         return solution;
     }
 };
