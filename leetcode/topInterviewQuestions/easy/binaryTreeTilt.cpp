ce8501d0e106275b370486030889b3662c1fad5c
Pedro <pedro.mendes.unicamp@gmail.com>
2018-10-25 01:04:02 +0000

    adding binaryTreeTilt

---
+++ leetcode/topInterviewQuestions/easy/binaryTreeTilt.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int sumNodes(TreeNode* root) {
+        if (root==nullptr)
+            return 0;
+        return root->val + sumNodes(root->right) + sumNodes(root->left);
+    }
+    
+    int tilt (TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        return abs(sumNodes(root->right)-sumNodes(root->left));
+    }
+    
+    int findTilt (TreeNode* root) {
+        if (root==nullptr)
+            return 0;
+        return tilt(root)+findTilt(root->left)+findTilt(root->right);
+    }
+};
+
+/*Optimization*/
+
+class Solution {
+public:
+    int sumNodes(TreeNode* root, int &tilt) {
+        if (root==nullptr)
+            return 0;
+        int sumRight = sumNodes(root->right,tilt);
+        int sumLeft = sumNodes(root->left,tilt);
+        tilt += abs(sumRight-sumLeft);
+        return root->val + sumRight + sumLeft;
+    }
+    
+    int findTilt (TreeNode* root) {
+        if (root==nullptr)
+            return 0;
+        int tilt = 0;
+        tilt+= abs(sumNodes(root->left,tilt)-sumNodes(root->right,tilt));
+        return tilt;
+    }
+};
