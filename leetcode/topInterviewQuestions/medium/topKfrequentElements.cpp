7632ec2d828e82bc0b07111dcaf09be9fda9a502
Pedro <pedro.mendes.unicamp@gmail.com>
2018-11-02 00:37:01 +0000

    adding topKfrequent elements

--- leetcode/topInterviewQuestions/medium/topKfrequentElements.cpp
+++ leetcode/topInterviewQuestions/medium/topKfrequentElements.cpp
@@ -1,17 +1,24 @@
+/* https://leetcode.com/problems/top-k-frequent-elements/
+git@Pedro-Mendes */
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> frequency;
-        vector< vector<int> > bucket(nums.size());
+        int bucketIndexSize;
+        int numsSize = nums.size();
+        vector< vector<int> > bucket(numsSize+1, vector<int> ());
         vector<int> output;
-        for(int i = 0; i < nums.size(); i++)
+        for(int i = 0; i < numsSize; i++)
             frequency[nums[i]]++;
         for(unordered_map<int,int>::iterator it = frequency.begin(); it != frequency.end(); it++)
             bucket[it->second].push_back(it->first);
-        for(int i = bucket.size(); i >= 0 && k > 0; i--){
-            if(bucket[i].size()){
-                output.push_back(bucket[i]);
+        for(int i = numsSize; i > 0 && k > 0;){
+            if(bucket[i].size()>0){
+                output.push_back(bucket[i][bucket[i].size()-1]);
+                bucket[i].pop_back();
                 k--;
+            }else{
+                i--;
             }
         }
         return output;
