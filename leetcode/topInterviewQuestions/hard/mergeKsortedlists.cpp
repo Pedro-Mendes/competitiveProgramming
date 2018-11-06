61f30c83060f105f2dcb0211f3e64e9ec7b02a85
Pedro <pedro.mendes.unicamp@gmail.com>
2018-11-06 00:46:16 +0000

    adding alternative solution to mergeKSortedLists

--- leetcode/topInterviewQuestions/hard/mergeKsortedlists.cpp
+++ leetcode/topInterviewQuestions/hard/mergeKsortedlists.cpp
@@ -31,3 +31,49 @@ public:
         return head->next;
     }
 };
+
+/*Using mergeTwoLists */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (!lists.size())
+            return nullptr;
+        ListNode* head = lists[0];
+        for(int i = 1; i < lists.size(); i++) {
+            head = mergeTwoSortedLists(head, lists[i]);
+        }
+        return head;
+    }
+    
+private:
+    ListNode* mergeTwoSortedLists(ListNode* L1, ListNode* L2) {
+        ListNode* head = new ListNode(0);
+        ListNode* dummy = head;
+        while (L1 != nullptr && L2!= nullptr) {
+            if (L2->val > L1->val) {
+                dummy->next = L1;
+                dummy = dummy->next;
+                L1 = L1->next;
+            } else {
+                dummy->next = L2;
+                dummy = dummy->next;
+                L2 = L2->next;
+            }
+        }
+        if (L1 == nullptr) {
+            dummy->next = L2;
+        } else {
+            dummy->next = L1;
+        }
+        return head->next;
+    }
+};
