d0d61ad3187ef09f0029f332c102d33e0280b720
Pedro <pedro.mendes.unicamp@gmail.com>
2019-02-14 01:19:35 +0000

    Adding problem 118-pascalsTriangle

---
+++ leetcode/topInterviewQuestions/easy/118-pascalsTriangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> solution;
+        for (int i = 0; i < numRows; i++) {
+            solution.push_back(vector<int> (i+1,1));
+            for (int j = 1; j < i; j++) 
+                solution[i][j] = solution[i-1][j-1] + solution[i-1][j];
+        }
+        return solution;
+    }
+};
