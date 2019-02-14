5112babe404b76301442b21c2c4e69e4e5f6f39c
Pedro Dias Ambrósio Mendes <pedrod@lnxcit021923.cit>
2019-02-14 13:01:48 -0200

    Adding problem 202-happyNumber.cpp

---
+++ leetcode/topInterviewQuestions/easy/202-happyNumber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    bool isHappyCount(int n, unordered_map<int,int> &checked) {
+        int tmp = 0;
+        while (n) {
+            tmp += pow(n%10,2);
+            n /= 10;
+        }
+        if (tmp == 1)
+            return true;
+        if(checked[tmp])
+            return false;
+        checked[tmp] = 1;
+        return isHappyCount(tmp, checked);
+    }
+public:
+    bool isHappy(int n) {
+        unordered_map<int, int> checked;
+        return isHappyCount(n, checked);
+    }
+};
