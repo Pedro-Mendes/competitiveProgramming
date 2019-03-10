27b1949f7927a9c60c9b5b1c61440e59f769b273
Pedro <pedro.mendes.unicamp@gmail.com>
2019-03-10 19:39:22 +0000

    Adding problem 155

---
+++ leetcode/topInterviewQuestions/easy/155-minStack.cpp
@@ -0,0 +1,46 @@
+/*https://leetcode.com/problems/min-stack
+git@Pedro-Mendes*/
+
+class MinStack {
+    private:
+        struct myStack {
+            int value;
+            myStack *next;
+        };
+        int minVal;
+        myStack *s;
+    public:
+        /** initialize your data structure here. */
+        MinStack() {
+            s = nullptr;
+            minVal = INT_MAX;
+        }
+
+        void push(int x) {
+            myStack *newNode = new myStack();
+            newNode->value = x;
+            newNode->next = s;
+            s = newNode;
+            minVal = min(minVal, x);
+        }
+
+        void pop() {
+            if (minVal == s->value) {
+                myStack *current = s->next;
+                minVal = INT_MAX;
+                while (current != nullptr) {
+                    minVal = min(minVal, current->value);
+                    current = current->next;
+                }
+            }
+            s = s->next;
+        }
+
+        int top() {
+            return s->value;
+        }
+
+        int getMin() {
+            return minVal;
+        }
+};
